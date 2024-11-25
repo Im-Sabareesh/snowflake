ee122818d51f6af12addaa45efa5b5fdbe94230e
sabareesh <saba@gmail.com>
2024-11-25 22:41:24 +0530

    sql file

---
+++ sql/snow.sql
@@ -0,0 +1,15 @@
+create or replace table git (id number, name varchar);
+
+insert into git values
+(1, 'saba'),
+(2, 'susi'),
+(3, 'ajay');
+
+create stream gitstream on table git;
+
+create task t
+warehouse = compute_wh
+schedule = '1 minute'
+as
+insert into git values
+(4, 'vijay');
