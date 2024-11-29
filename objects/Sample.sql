956fdc0a89ee2e5c5eae2e0254c92c8c0b02485d
sabareesh <saba@gmail.com>
2024-11-29 18:39:36 +0530

    test file added

--- objects/Sample.sql
+++ objects/Sample.sql
@@ -1026,4 +1026,4 @@ INSERT INTO `user_details` (`user_id`, `username`, `first_name`, `last_name`, `g
 (999, 'wright26', 'morgan', 'cooper', 'Male', 'e8c529514ec002b6bb1ce139adc2eaf8', 1),
 (1000, 'james81', 'daniel', 'brown', 'Female', '6ce961f58c6f12943531080a444f570d', 1);
 
-create table test2222 (id number, name varchar);
+create table test222 (id number, name varchar);
