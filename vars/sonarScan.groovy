fa199faf4d74634a766a3659e35ec8146d21f167
sabareesh <saba@gmail.com>
2025-03-07 15:07:23 +0530

    echo

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,11 +1,5 @@
 def call(String sonarEnvName = 'sonar') {
-    stage('Run SonarQube Scan') {  // No need for `stages` since it’s inside a pipeline
-        steps {
-            script {
                 withSonarQubeEnv(sonarEnvName) {
                     sh "${env.SCANNER_HOME}/bin/sonar-scanner -Dsonar.login=${env.SONARQUBE_TOKEN}"
-                }
-            }
-        }
     }
 }
