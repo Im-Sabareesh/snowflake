f46b1a2050e5f62499bf553abcb3bfb92e436687
sabareesh <saba@gmail.com>
2025-03-07 15:27:18 +0530

    echo

--- vars/qualityGateCheck.groovy
+++ vars/qualityGateCheck.groovy
@@ -1,5 +1,5 @@
 def call() {
-timeout(time: 2, unit: 'MINUTES') {  // Wait for the Quality Gate result
+timeout(time: 5, unit: 'MINUTES') {  // Wait for the Quality Gate result
                         def qg = waitForQualityGate()
                         if (qg.status != 'OK') {
                             error "Pipeline failed due to Quality Gate failure: ${qg.status}"
