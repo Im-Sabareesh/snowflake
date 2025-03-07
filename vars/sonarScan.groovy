bc15e1bf94beb7fa99bc8cccc7a47c0a4829bfdf
sabareesh <saba@gmail.com>
2025-03-07 15:12:04 +0530

    echo

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,4 +1,4 @@
-def call(String sonarEnvName = 'sonar') {
+def call(String sonarEnvName) {
                 withSonarQubeEnv(sonarEnvName) {
                     sh "${env.SCANNER_HOME}/bin/sonar-scanner -Dsonar.login=${env.SONARQUBE_TOKEN}"
     }
