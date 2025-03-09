56267ee5516f5a4652a36153f005c59221a4d27f
sabareesh <saba@gmail.com>
2025-03-09 19:55:26 +0530

    echo

--- vars/ecrAuth.groovy
+++ vars/ecrAuth.groovy
@@ -1,4 +1,4 @@
 def call() {
- sh 'aws ecr get-login-password --${env.REGION}  | docker login --username AWS --password-stdin ${env.ACCOUNT}.dkr.ecr.${env.REGION}.amazonaws.com'
+ sh 'aws ecr get-login-password --region ${env.REGION}  | docker login --username AWS --password-stdin ${env.ACCOUNT}.dkr.ecr.${env.REGION}.amazonaws.com'
 }
 
