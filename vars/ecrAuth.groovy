4066f90b14389d5e871abf2fe62b19c96b5c95d5
sabareesh <saba@gmail.com>
2025-03-09 20:04:45 +0530

    echo

--- vars/ecrAuth.groovy
+++ vars/ecrAuth.groovy
@@ -1,4 +1,4 @@
-def call(string region, string account) {
+def call(String region, String account) {
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
 }
 
