b846f74504d4bcd8743283ce4e49536e4c2a0798
sabareesh <saba@gmail.com>
2025-03-09 20:25:31 +0530

    echo

--- vars/ecrAuth.groovy
+++ vars/ecrAuth.groovy
@@ -1,4 +1,9 @@
 def call() {
+try {
+echo "Authenticating with ECR"
 sh 'aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com'
+echo "Successfully logged into AWS ECR."
+}catch (Exception e) {
+        error "ECR login failed: ${e.message}"
+    }
 }
-
